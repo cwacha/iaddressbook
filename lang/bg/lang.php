134f0d8afa11bd2cf7fb9ed24d32f668420838e9
Clemens Wacha <clemens@wacha.ch>
2008-04-22 20:32:58 +0000

    added Bulgarian language file

---
+++ lang/bg/lang.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * bulgarian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Vladimir Vitkov <vvitkov@gmail.com>
+ */
+
+$lang['encoding']             = 'utf-8';
+$lang['direction']            = 'ltr';
+
+// Letter sort rules
+$lang['sort_rules_from']                = 'Ä,ä,Ö,ö,Ü,ü,É,é,È,è,À,à';
+$lang['sort_rules_to']                  = 'A,a,O,o,U,u,E,e,E,e,A,a';
+
+// Buttons
+$lang['btn_edit']             = 'Редактирай';
+$lang['btn_delete']           = 'Изтрий контакт';
+$lang['btn_search']           = 'Търси';
+$lang['btn_cancel']           = 'Откажи';
+$lang['btn_save']             = 'Запази';
+$lang['btn_vcardexport']      = 'Изнеси като vCard';
+$lang['btn_vcardimport']      = 'Внеси vCard';
+$lang['btn_login']            = 'Вход';
+$lang['btn_logout']           = 'Изход';
+
+// Questions
+$lang['confirm_del_contact']  = 'Искате ли да изтриете този контакт?';
+$lang['confirm_del_contacts'] = 'Искате ли да изтриете избраните контакти?';
+$lang['confirm_cat_remove_contacts'] = 'Искате ли да премахнете избраните контакти от категорията?';
+$lang['confirm_cat_delete']   = 'Искате ли да изтриете тази категория?';
+
+
+// General Text
+$lang['contacts']             = 'Контакти';
+$lang['no_contacts']          = 'няма контакти';
+$lang['login']                = 'Вход';
+$lang['username']             = 'Потр. Име';
+$lang['password']             = 'Парола';
+$lang['sticky']               = 'Запомни';
+$lang['wrong_userpass']       = 'Грешно име или парола';
+$lang['action_not_allowed']   = 'Нямате право да извършите избраното действие';
+$lang['logged_in_as']         = 'Влезнал като';
+$lang['category']             = 'Категория';
+$lang['category_all']         = 'Всички';
+$lang['cat_add_to']           = 'добави към';
+$lang['cat_delete_from']      = 'изтрий от';
+$lang['cat_add']              = 'създай категория';
+$lang['cat_delete']           = 'изтрий категория';
+$lang['cat_lastimport']       = 'Последно внесени';
+$lang['cat_changed']          = 'Променени контакти';
+$lang['delete_contacts']      = 'изтрий контакт(и)';
+$lang['import_export']        = 'Внасяне/Изнасяне';
+$lang['export_vcard']         = 'изнеси като vCard';
+$lang['export_csv']           = 'изнеси в CSV формат';
+$lang['export_ldif']          = 'изнеси в LDIF формат';
+$lang['import_folder']        = 'внеси от папка';
+$lang['create_contact']       = 'нов контакт';
+$lang['map']                  = 'Карта';
+$lang['select_action']        = 'избери действие...';
+
+// Person Labels
+$lang['no_name']              = '(Без Име)';
+$lang['label_custom']         = 'Специално...';
+$lang['label_customprompt']   = 'Добавяне на нов специален етикет';
+$lang['label_work']           = 'работа';
+$lang['label_home']           = 'домашен';
+$lang['label_cell']           = 'мобилен';
+$lang['label_pager']          = 'пейджър';
+$lang['label_main']           = 'основен';
+$lang['label_workfax']        = 'факс работа';
+$lang['label_homefax']        = 'факс домашен';
+
+$lang['label_other']          = 'друг';
+$lang['label_homepage']       = 'WEB страница';
+$lang['label_friend']         = 'приятел';
+$lang['label_assistant']      = 'асистент';
+$lang['label_father']         = 'баща';
+$lang['label_mother']         = 'майка';
+$lang['label_parent']         = 'родител';
+$lang['label_brother']        = 'брат';
+$lang['label_sister']         = 'сестра';
+$lang['label_child']          = 'дете';
+$lang['label_spouse']         = 'годеница';
+$lang['label_partner']        = 'партньор';
+$lang['label_manager']        = 'мениджър';
+
+$lang['label_birthday']       = 'Рожден Ден';
+$lang['label_notes']          = 'бележки';
+$lang['label_updated']        = 'обновен: ';
+$lang['label_nocontact']      = 'Няма избран контакт';
+
+$lang['label_title']          = 'Предст.';
+$lang['label_firstname']      = 'Име';
+$lang['label_firstname2']     = 'Презиме';
+$lang['label_lastname']       = 'Фамилия';
+$lang['label_suffix']         = 'Наст.';
+$lang['label_nickname']       = 'Прякор';
+$lang['label_phoneticfirstname'] = 'Фонетично име';
+$lang['label_phoneticlastname']  = 'Фонетична фамилия';
+$lang['label_jobtitle']       = 'Позиция';
+$lang['label_department']     = 'Отдел';
+$lang['label_organization']   = 'Компания';
+$lang['label_photoremove']    = 'Изтрий';
+$lang['label_photochange']    = 'Промени';
+
+$lang['label_phones']         = 'Тлефони';
+$lang['label_emails']         = 'Е-Мейл адреси';
+$lang['label_urls']           = 'Страници';
+$lang['label_relatednames']   = 'Популярни имена';
+$lang['label_chathandles']    = 'ИМ';
+$lang['label_addresses']      = 'Адреси';
+
+$lang['label_street']         = 'Улица';
+$lang['label_zip']            = 'П.К.';
+$lang['label_city']           = 'Град';
+$lang['label_state']          = 'Област';
+$lang['label_country']        = 'Страна';
+
+// Birthday Reminder
+$lang['birthdays']            = 'Наближаващи рожденни дни';
+$lang['bday_none']            = 'няма';
+$lang['bday_month']           = 'следващият $n месец';
+$lang['bday_months']          = 'следващите $n месеци';
+$lang['bday_week']            = 'следващата $n седмица';
+$lang['bday_weeks']           = 'следващите $n weeks';
+$lang['bday_day']             = 'следващите $n ден';
+$lang['bday_day2']            = 'следващите $n дни';
+$lang['bday_days']            = 'следващите $n дни';
+$lang['bday_today']           = 'днес!';
+
+// Date Helpers
+$lang['month'][1]             = 'Януари';
+$lang['month'][2]             = 'Февруари';
+$lang['month'][3]             = 'Март';
+$lang['month'][4]             = 'Април';
+$lang['month'][5]             = 'Май';
+$lang['month'][6]             = 'Юни';
+$lang['month'][7]             = 'Юли';
+$lang['month'][8]             = 'Август';
+$lang['month'][9]             = 'Септември';
+$lang['month'][10]            = 'Октомври';
+$lang['month'][11]            = 'Ноември';
+$lang['month'][12]            = 'Декември';
+
+?>
