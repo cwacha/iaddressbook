2fb95065e16a24d9754d7961b38dfbfc2c3cfebc
Clemens Wacha <clemens@wacha.ch>
2012-02-14 22:07:02 +0000

    restructured folders to facilitate packaging

---
+++ pkg/pkgmake.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+
+all() {
+	import && pkg
+}
+
+import() {
+	echo "##### importing"
+	[ -d BUILD ] && rm -rf BUILD
+	mkdir BUILD
+	cp -r ../src/* BUILD
+	find . -depth -name ".DS_Store" -exec rm {} \;
+	find . -depth -name ".svn" -exec rm {} \;
+	rm BUILD/conf/config.php
+	rm BUILD/conf/auth.php
+}
+
+pkg() {
+	echo "##### packaging"
+	VERSION=`head -1 BUILD/VERSION | sed 's/[ \t]/_/g'`
+	PKG=iaddressbook-$VERSION
+	mv BUILD $PKG
+	tar cfz $PKG.tar.gz $PKG
+}
+
+clean() {
+	echo "##### cleaning"
+	rm -rf BUILD
+	rm -rf iaddressbook-*
+}
+
+usage() {
+	echo "Usage: $0 <action>"
+	echo
+	echo "ACTIONS:"
+	declare -F | awk '{print $3}' | grep -v usage
+}
+
+if [ $# -eq 0 ]; then
+	usage
+	exit
+fi
+
+$*
+echo "##### done."
+
