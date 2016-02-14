4d11ef9c8a5b0360f1f2861a08d4be88d9bcef4a
Clemens Wacha <clemens@wacha.ch>
2016-02-14 16:50:19 +0000

    upgrading SabreDAV step 1/2: removing old SaberDAV

--- src/lib/php/SabreDAV/examples/sql/sqlite.propertystorage.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE propertystorage (
-    id integer primary key asc,
-    path TEXT,
-    name TEXT,
-    value TEXT
-);
-
-
-CREATE UNIQUE INDEX path_property ON propertystorage (path, name);
